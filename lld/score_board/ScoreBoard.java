f94711660e28662647d64abb612147f864c6913d
Gaurav <sgau0522@gmail.com>
2022-06-28 07:05:09 +0530

    Update ScoreBoard.java

--- lld/score_board/ScoreBoard.java
+++ lld/score_board/ScoreBoard.java
@@ -10,30 +10,35 @@ class Score{
     Integer oversBalled;
     Integer wicketsTaken;
     Integer wideBalls;
-    Integer NoBalls;
+    Integer noBalls;
+
+    Score(){
+        this.runScored = 0;
+        this.ballsFaced = 0;
+        this.numOfFours = 0;
+        this.numOfSixes = 0;
+        this.oversBalled = 0;
+        this.wicketsTaken = 0;
+        this.wideBalls = 0;
+        this.noBalls = 0;
+    }
 }
 
 public class ScoreBoard{
     Integer totalScore;
     HashMap<Player, Score> board;
 
-    void addScore(Integer run){
+    ScoreBoard(){
+        this.totalScore = 0;
+        this.board = new HashMap<>();
+    }
+
+    void addScore(Player batsman){
+        Score score = this.board.getOrDefault(batsman, new Score());
 
     }
 
-    void addWicket(){
-
-    }
-
-    void newBowler(Player newBowler){
-
-    }
-
-    void newBatsman(Player newBatsman){
-
-    }
-
-    void swapBatsman(){
+    void addWicket(Player bowler){
 
     }
 }
