f6e7aba7f932dd1098e69f43e85aa88523293615
Gaurav <sgau0522@gmail.com>
2022-06-28 16:13:27 +0530

    Update VehicleFilter.java

--- lld/vehicle_rental/booking/VehicleFilter.java
+++ lld/vehicle_rental/booking/VehicleFilter.java
@@ -1,32 +1,40 @@
-package lld.vehicle_rental.booking;
+package booking;
 
 import java.util.*;
 
-import lld.vehicle_rental.branch.Branch;
-import lld.vehicle_rental.branch.BranchManager;
-import lld.vehicle_rental.vehicle.*;
+import branch.Branch;
+import branch.BranchManager;
+import vehicle.*;
 
+/*
+ * Holds filters which can be applied while booking
+ * Can hold new filters for future usecases
+ */
 public class VehicleFilter {
     public enum Filter {
         LowestRentalPrice
     }
 
-    public static List<Vehicle> filter(List<Vehicle> vehicles, Filter filter) {
+    public static List<Vehicle> filter(List<Vehicle> vehicles, Filter filter, VehicleCategory category) {
         List<Vehicle> selectedVehicles = new ArrayList<Vehicle>();
 
         switch (filter) {
             case LowestRentalPrice: {
                 Integer minPrice = Integer.MAX_VALUE;
                 for (int i = 0; i < vehicles.size(); i++) {
-                    Branch b = BranchManager.getBranchManager().getBranch(vehicles.get(i).getBranch());
-                    Integer price = b.getVehicleCategoryPrice(vehicles.get(i).getCategory());
+                    Vehicle vehicle = vehicles.get(i);
+                    if(vehicle.getCategory()!=category)
+                        continue;
+
+                    Branch b = BranchManager.getBranchManager().getBranch(vehicle.getBranch());
+                    Integer price = b.getVehicleCategoryPrice(vehicle.getCategory());
 
                     if (minPrice > price) {
                         selectedVehicles.clear();
                         minPrice = price;
-                        selectedVehicles.add(vehicles.get(i));
+                        selectedVehicles.add(vehicle);
                     } else if (minPrice == price) {
-                        selectedVehicles.add(vehicles.get(i));
+                        selectedVehicles.add(vehicle);
                     }
                 }
 
