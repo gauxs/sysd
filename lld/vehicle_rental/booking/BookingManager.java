99086b3438887b09205241aaee1bb87e403f29a6
Gaurav <sgau0522@gmail.com>
2022-06-28 16:13:10 +0530

    Update BookingManager.java

--- lld/vehicle_rental/booking/BookingManager.java
+++ lld/vehicle_rental/booking/BookingManager.java
@@ -1,26 +1,34 @@
-package lld.vehicle_rental.booking;
+package booking;
 
 import java.util.*;
-import lld.vehicle_rental.vehicle.*;
+import vehicle.*;
+import exceptions.*;
 
+/*
+ * Holds filter for booking
+ * Facilitates booking
+ */
 public class BookingManager {
-    public static Booking bookVehicle(Integer startTime, Integer endTime, VehicleCategory category) {
+    public static Booking bookVehicle(Integer startTime, Integer endTime, VehicleCategory category) throws BookingException{
         // get all vehicles from Inventory
         VehicleManager vm = VehicleManager.getVehicleManager();
         List<Vehicle> vehicles = vm.getAvailaibility(startTime, endTime);
 
         // filter vehicles
-        List<Vehicle> filteredVehicles = VehicleFilter.filter(vehicles, VehicleFilter.Filter.LowestRentalPrice);
+        List<Vehicle> filteredVehicles = VehicleFilter.filter(vehicles, VehicleFilter.Filter.LowestRentalPrice, category);
 
         // select a vehicle based on filter
+        if(filteredVehicles.size()==0)
+            throw new BookingException("No vehicle of type "+ category + " availaible!!");
+
         Vehicle choosenVehicle = filteredVehicles.get(0);
 
         // update Inventory and block time slot for this vehicle
         vm.blockVehicle(choosenVehicle.getID(), startTime, endTime);
 
         // create a new booking
-        Booking b = new Booking(choosenVehicle.getID(), startTime, endTime);
+        Booking booking = new Booking(choosenVehicle.getID(), startTime, endTime);
 
-        return b;
+        return booking;
     }
 }
